7cd51f30e55f45843157492df8eb9714cdf0413f
Svyatoslav <sheinsvyatoslav@gmail.com>
2021-11-04 21:42:55 +0300

    eng lang added

---
+++ scripts/initial-cards-en.js
@@ -0,0 +1,26 @@
+const initialCards = [
+  {
+    name: 'Arkhyz',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Chelyabinsk Oblast',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Ivanovo',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Kamchatka',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Kholmogorsky District',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Baikal',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+]; 
